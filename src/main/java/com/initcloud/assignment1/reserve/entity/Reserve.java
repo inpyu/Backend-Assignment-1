fb47d504b0f9da9112c834367b830f49af3da5f1
잉퓨 <dayeon620@kakao.com>
2023-07-12 10:47:35 +0900

    Feat: #3 Room, Reserve, User 엔티티 개발

--- src/main/java/com/initcloud/assignment1/reserve/entity/Reserve.java
+++ src/main/java/com/initcloud/assignment1/reserve/entity/Reserve.java
@@ -1,7 +1,24 @@
 package com.initcloud.assignment1.reserve.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Reserve {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Date startTime;
+
+	@Column(nullable = false)
+	private Date endTime;
+
 }
--- src/main/java/com/initcloud/assignment1/room/Room.java
+++ src/main/java/com/initcloud/assignment1/room/Room.java
@@ -1,4 +1,27 @@
 package com.initcloud.assignment1.room;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Room {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Size size;
+
 }
