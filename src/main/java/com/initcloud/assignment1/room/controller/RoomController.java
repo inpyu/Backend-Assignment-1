010a8ecaaee1ee5f15f6f74280672bebefd2842b
잉퓨 <dayeon620@kakao.com>
2023-07-14 10:46:59 +0900

    Feat: #5 모든 회의실 조회 API

--- src/main/java/com/initcloud/assignment1/room/controller/RoomController.java
+++ src/main/java/com/initcloud/assignment1/room/controller/RoomController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.initcloud.assignment1.common.SuccessResponse;
+import com.initcloud.assignment1.room.dto.RoomAllListOutDTO;
 import com.initcloud.assignment1.room.dto.RoomListOutDTO;
 import com.initcloud.assignment1.room.service.RoomService;
 
@@ -32,4 +33,13 @@ public class RoomController {
 	public SuccessResponse<List<RoomListOutDTO>> detailRoom(@PathVariable Long roomId){
 		return new SuccessResponse<>(SUCCESS, roomService.getRoomDetail(roomId));
 	}
+
+	/**
+	 * 모든 회의실 조회
+	 * */
+	@GetMapping("/all")
+	public SuccessResponse<List<RoomAllListOutDTO>> allRoom(){
+		return new SuccessResponse<>(SUCCESS, roomService.getAllRooms());
+	}
+
 }
